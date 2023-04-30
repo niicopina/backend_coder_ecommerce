d054dc1ca6ba37278a06c62df34a246cc60f0775
nicopina <pinanicolasagustin@gmail.com>
2023-04-30 16:43:29 -0300

    query_function req

--- server.js
+++ server.js
@@ -25,9 +25,35 @@ let one_function = (req, res) => {
     //console.log(typeof id)
     let one = pm.getProductsById(id)
     console.log(one)
-    return res.send({
-        success: true,
-        product: one
-    })
+    if(one){
+        return res.send({
+            success: true,
+            product: one
+        })
+    } else {
+        return res.send({
+            success: false,
+            product: 'No product with this ID!'
+        })
+    }
 }
 server.get(one_route, one_function)
+
+let query_route = '/products'
+let query_function = (req, res) => {
+    console.log(req.query)
+    let quantity = req.query.quantity ?? 3
+    let products = pm.getProducts().slice(0, quantity)
+    if(products.length > 0){
+        return res.send({
+            success: true,
+            products: products
+        })
+    } else {
+        return res.send({
+            success: false,
+            products: 'products not found'
+        })
+    }
+    }
+server.get(query_route, query_function)
