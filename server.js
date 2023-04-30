4e3c03dbbe78b8140bb791f07e33d23cd5a97820
nicopina <pinanicolasagustin@gmail.com>
2023-04-30 18:01:48 -0300

    server routes /

--- server.js
+++ server.js
@@ -17,34 +17,43 @@ let index_function = (req, res) => {
 }
 server.get(index_route, index_function)
 
-
-let products_route = '/products'
-let products_function = (req, res) => {
-    let products = pm.getProducts()
-    return res.send(products)
-}
-server.get(products_route, products_function)
-
 let one_route = '/products/:id'
 let one_function = (req, res) => {
-    let parametros = req.params
-    let id = parametros.id
-    let product = pm.getProductsById(id)
-    if (!product) {
+    let parametros = req.parametros
+    let id = Number(parametros.id)
+    //console.log(id)
+    //console.log(typeof id)
+    let one = pm.getProductsById(id)
+    console.log(one)
+    if(one){
+        return res.send({
+            success: true,
+            product: one
+        })
+    } else {
         return res.send({
             success: false,
-            message: `Product with id ${id} not found`
+            product: 'No product found with this ID'
         })
     }
-    return res.send({
-        success: true,
-        product: product
-    })
-    
 }
 server.get(one_route, one_function)
 
-server.post(
-    '/products',
-    (req, res)
-)
+let query_route = '/products'
+let query_function = (req, res) => {
+    console.log(req.query)
+    let quantity = res.query.quantity ?? 3
+    let products = pm.getProducts().slice(0, quantity)
+    if(products.length > 0){
+        return res.send({
+            success: true,
+            products: products
+        })
+    } else {
+        return res.send({
+            success: false,
+            products: 'Products not Found!'
+        })
+    }
+}
+server.get(query_route, query_function)
---
+++ src/products.js
@@ -0,0 +1,96 @@
+import fs from 'fs'
+
+class ProductManager {
+    constructor(path){
+        this.path = path
+        this.nextId = 1
+        if(!fs.existsSync(path)){
+            fs.writeFileSync(path, JSON.stringify([]))
+        }
+    }
+    addProduct(product){
+        const products = this.getProductsFromFile()
+        if(!product.title || !product.description || !product.price || !product.thumbnail ||
+            !product.code || !product.stock){
+                console.error('Every camp is required')
+            }
+        if(products.some(p => p.code === product.code)){
+            console.error('A product with the same code already exist')
+            return
+        }
+        product.id = this.nextId++
+        products.push(product)
+        this.saveProductsToFile(products)
+    }
+    getProducts(){
+        return this.getProductsFromFile()
+    }
+    getProductsById(id){
+        const products = this.getProductsFromFile()
+        const product = products.find(p => p.id === id)
+        if(!product){
+            console.error('Product not found')
+            return null
+        }
+        return product
+    }
+    updateProduct(id, updatedProduct){
+        const products = this.getProductsFromFile()
+        const index = products.findIndex(p => p.id === id)
+        if(index === -1){
+            console.error('Product not found')
+            return
+        }
+        products[index] = {
+            ...products[index],
+            ...this.updateProduct,
+            id: id
+        }
+        this.saveProductsToFile(products)
+    }
+    deleteProduct(id){
+        const products = this.getProductsFromFile()
+        const index = products.findIndex(p => p.id === id)
+        if(index === -1){
+            console.error('Product not found')
+            return
+        }
+        products.splice(index, 1)
+        this.saveProductsToFile(products)
+    }
+    getProductsFromFile(){
+        const data = fs.readFileSync(this.path, 'utf-8')
+        return JSON.parse(data)
+    }
+    saveProductsToFile(products){
+        fs.writeFileSync(this.path, JSON.stringify(products))
+    }
+}
+const pm = new ProductManager('./products.json')
+
+/* pm.addProduct({
+    title: 'Cheesecake',
+    description: 'Torta de queso con frutos rojos',
+    price: 4000,
+    thumbnail: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F21%2F2015%2F02%2F11%2Fcheesecake-facil-con-leche-condensada-2000.jpg',
+    code: 'PROD1',
+    stock: 4
+})
+pm.addProduct({
+    title: 'Torta Oreo',
+    description: 'Torta chocolate y mousse a base de galletas oreo',
+    price: 5000,
+    thumbnail: 'https://pasteleriadc.com/wp-content/uploads/torta-dripcake-helado-768x1078.jpg',
+    code: 'PROD2',
+    stock: 2
+});
+pm.addProduct({
+    title: 'Lemon Pie',
+    description: 'Pastel de mousse de limon con merengue',
+    price: 4500,
+    thumbnail: 'https://img2.rtve.es/i/?w=1600&i=1635859279860.jpg',
+    code: 'PROD3',
+    stock: 5
+}) */
+
+export default pm
