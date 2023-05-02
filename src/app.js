29e574c6a9613a3025f626e788992de8d9745656
nicopina <pinanicolasagustin@gmail.com>
2023-05-02 18:05:32 -0300

    app.js endpoints

--- src/app.js
+++ src/app.js
@@ -1,63 +1,51 @@
 import express from 'express'
-import ProductManager from './products.js'
+import fs from 'fs'
 
 const app = express()
-const pm = new ProductManager('./products.json')
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
-
-//encontrar todos los productos
-app.get('products', (req, res) => {
-    const products = pm.getProducts()
-    res.json(products)
-})
-//encontrar producto por id
-app.get('./products/:id', (req, res) => {
-    const productId = Number(req.params.id)
-    const product = pm.getProductsById(productId)
-    if(product) {
-        return res.send({
-            success: true,
-            product: product
-        })
-    } else {
-        return res.send({
+app.get('/api/products', async (req, res)=> {
+    try{
+        const limit = req.query.limit ? parseInt(req.query.limit) : null
+        const products = JSON.parse(await fs.promises.readFile('./productos.txt', 'utf-8'))
+        if(limit){
+            res.send({
+                success: true,
+                response: products.slice(0, limit)
+            })
+        }else{
+            res.send({
+                success: true,
+                response: []
+            })
+        }
+    }catch(error) {
+        console.log(error)
+        res.send({
             success: false,
-            product: 'product not found'
+            response: []
         })
     }
 })
-//crear nuevo producto
-app.post('/products', (req, res) => {
-    const newProduct = req.body
-    pm.addProduct(newProduct)
-    return res.send({
-        success: true,
-        message: 'Product created successfully'
-    })
-})
-//actualizar producto
-app.put('/products/:id', (req, res) => {
-    const productId = Number(req.params.id)
-    const updatedProduct = req.body
-    pm.updateProduct(productId, updatedProduct)
-    return res.send({
-        success: true,
-        message: 'Product updated successfully'
-    })
-})
-app.delete('/products/:id', (req, res) => {
-    const productId = Number(req.params.id)
-    pm.deleteProduct(productId)
-    return res.send({
-        success: true,
-        message: 'Product deleted successfully'
-    })
-})
-
-//server
-const PORT = 8000
-app.listen(port, () => {
-    console.log(`Server listening on port ${PORT}`)
+app.get('/api/products/:pid', async (req, res)=> {
+    try{
+        const products = JSON.parse(await fs.promises.readFile('./productos.txt', 'utf-8'))
+        const product = products.find(p => p.id === req.params.pid)
+        if(!product) {
+            res.send({
+                success: false,
+                response: {}
+            })
+        }else{
+            res.send({
+                success: true,
+                response: product
+            })
+        }
+    }catch(error){
+        console.log(error)
+        res.send({
+            success: false,
+            response: {}
+        })
+    }
 })
