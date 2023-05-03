7c875836c3bdd326093bef09f4569d2fd61f9beb
nicopina <pinanicolasagustin@gmail.com>
2023-05-03 16:56:38 -0300

    server.post

--- server.js
+++ server.js
@@ -19,10 +19,8 @@ server.get(index_route, index_function)
 
 let one_route = '/products/:id'
 let one_function = (req, res) => {
-    let parametros = req.parametros
+    let parametros = req.params
     let id = Number(parametros.id)
-    //console.log(id)
-    //console.log(typeof id)
     let one = productManager.getProductsById(id)
     console.log(one)
     if(one){
@@ -41,8 +39,7 @@ server.get(one_route, one_function)
 
 let query_route = '/products'
 let query_function = (req, res) => {
-    console.log(req.query)
-    let quantity = res.query.quantity ?? 3
+    let quantity = req.query.quantity ?? 3
     let products = productManager.getProducts().slice(0, quantity)
     if(products.length > 0){
         return res.send({
@@ -57,6 +54,31 @@ let query_function = (req, res) => {
     }
 }
 server.get(query_route, query_function)
+
+server.post(
+    '/products',
+    (req, res) => {
+        let title = req.body.title ?? null
+        let description = req.body.description ?? null
+        let price = req.body.price ?? null
+        let thumbnail = req.body.thumbnail ?? null
+        let code = req.body.code ?? null
+        let stock = req.body.stock ?? null
+        if(title&&description&&price&&thumbnail&&code&&stock){
+            productManager.addProduct({title, description, price, thumbnail, code, stock})
+            return res.json({
+                status: 201,
+                message: 'Created'
+            })
+        } else {
+            res.json({
+                status: 400,
+                message: 'Check data!'
+            })
+        }
+    }
+)
+
 
 import cartManager from './src/cart.js'
 import app from './src/app.js'
