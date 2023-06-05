fad4bcaf143c032fb78216281e5d44478f81cda1
nicopina <pinanicolasagustin@gmail.com>
2023-06-05 17:52:25 -0300

    POST 1 carts

--- src/routes/api/carts.mongo.js
+++ src/routes/api/carts.mongo.js
@@ -9,6 +9,19 @@ carts_mongo.post(
     async(req,res,next)=> {
         try {
             const {quantity, product_id} = req.body
+            const product = await Product.findById(product_id)
+            if(!product){
+                return res.status(404).json({
+                    success: false,
+                    message: 'product not found'
+                })
+            }
+            const cart = await Cart.create({product_id, quantity})
+            return res.status(201).json({
+                success: true,
+                message: `Cart created, ID: ${cart._id}`,
+                data: cart
+            })
         } catch (error) {
             next(error)
         }
